4e09a2173c6342745b4a0f6c643eb8cac89cf98c
MinimumViablePerson <nicolas.marcora@yahoo.co.uk>
2018-10-11 13:09:05 +0100

    finished lecture

--- 27-fetch/index.js
+++ 27-fetch/index.js
@@ -1,8 +1,11 @@
+// get all the elements we'll need from the page
 const pokeList = document.querySelector('#pokemon-list')
 const pokeForm = document.querySelector('#pokemon-form')
 const pokeNameInput = document.querySelector('#pokemon-name')
 const pokeUrlInput = document.querySelector('#pokemon-url')
 
+// put a single pokemon on the page
+// this function takes a single object
 const renderPokemon = pokemon => {
   const pokeItem = document.createElement('li')
   pokeItem.innerHTML = `
@@ -12,18 +15,35 @@ const renderPokemon = pokemon => {
   pokeList.append(pokeItem)
 }
 
+// leverage the function we just wrote to add multiple pokemon to the page
+// this function takes an array of pokemon objects
 const renderPokemons = pokemons =>
   pokemons.forEach(renderPokemon)
 
+// call the getPokemons() function we wrote in promises.js
+// getPokemons returns a Promise with an array of pokemon objects
+// we can unwrap the Promise with `.then()` and use the array
 getPokemons()
+  // this does the same as: .then(pokemons => renderPokemons(pokemons))
   .then(renderPokemons)
 
+// the <form> on the page triggers a 'submit' event
 pokeForm.addEventListener('submit', event => {
+  // let's catch it and prevent it from refreshing the page
   event.preventDefault()
+
+  // now we want to create a new pokemon
+  // both on the server and on the screen
+  // we need a pokemon object for our functions to work
   const pokemon = {
     name: pokeNameInput.value,
     url: pokeUrlInput.value
   }
+
+  // let's post that to the server using the createPokemon()
+  // function we wrote in promises.js
   createPokemon(pokemon)
+    // when the server is done, we get the new pokemon
+    // and we can call renderPokemon() on it to render it on the page
     .then(renderPokemon)
 })
--- 27-fetch/promises.js
+++ 27-fetch/promises.js
@@ -1,26 +1,44 @@
 // Promises, promises...
 
+// fetch(url: String, ?options: Object)
+// fetch takes 2 parameters
+// - a url in String format
+// - options in Object format (optional)
+
+// GET all pokemons
 const getPokemons = () =>
+  // fetch makes a GET request by default
+  // so no options needed here
   fetch('http://localhost:3000/pokemons')
     .then(resp => resp.json())
 
+// GET a single pokemon by id
 const getPokemon = id =>
   fetch(`http://localhost:3000/pokemons/${id}`)
     .then(resp => resp.json())
 
+// POST a new pokemon to the server
 const createPokemon = pokemon =>
   fetch('http://localhost:3000/pokemons', {
+    // This time, we need to specify the method
+    // because we don't want a GET, but a POST
     method: 'POST',
+    // We tell the server what kind of data we'll be sending
     headers: { 'Content-Type': 'application/json' },
+    // We pass that data via a String in the *body* of the request
+    // JSON.stringify() takes some JavaScript
+    // and turns it into a JSON string
     body: JSON.stringify(pokemon)
   }).then(resp => resp.json())
 
+// DELETE a pokemon from the server by id
 const deletePokemon = id =>
   fetch(`http://localhost:3000/pokemons/${id}`, {
     method: 'DELETE',
     headers: { 'Content-Type': 'application/json' }
   }).then(resp => resp.json())
 
+// PATCH an existing pokemon
 const updatePokemon = pokemon =>
   fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
     method: 'PATCH',
